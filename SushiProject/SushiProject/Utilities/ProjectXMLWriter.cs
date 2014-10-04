80119a90c7cfaaafb2ed0de7c46dfe097449a322
Lynn <leelightman@gmail.com>
2014-10-04 12:48:25 -0400

    lililil

--- SushiProject/SushiProject/Utilities/ProjectXMLWriter.cs
+++ SushiProject/SushiProject/Utilities/ProjectXMLWriter.cs
@@ -53,8 +53,61 @@ namespace SushiProject.Utilities
             {
                 return;
             }
+
             XElement xml = new XElement("Project");
             xml.Add(new XElement("Name", gameProject.Name));
+            
+            ///<GameObject>Objects
+            XElement gameObjectsXml = new XElement("Objects");
+            foreach (GameObject go in gameProject.Objects)
+            {
+                XElement goXml = new XElement("Object");
+                goXml.Add(new XElement("Name", go.Name));
+                gameObjectsXml.Add(goXml);
+            }
+            xml.Add(gameObjectsXml);
+
+            ///<Level>Levels
+            XElement gameLevelsXml = new XElement("Levels");
+            foreach (Level go in gameProject.Levels){
+                XElement goLevXml = new XElement("Level");
+                goLevXml.Add(new XElement("Name", go.Name));
+
+                ///<LevelObject> levelObjects
+                foreach (LevelObject levelobjgo in go.levelObjects)
+                {
+                    XElement levelObjs = new XElement("LevelObject");
+                    levelObjs.Add(new XElement("X", levelobjgo.X));
+                    levelObjs.Add(new XElement("Y", levelobjgo.Y));
+                    levelObjs.Add(new XElement("Rotation", levelobjgo.Rotation));
+                    levelObjs.Add(new XElement("Class.Name", levelobjgo.Class.Name));
+                    goLevXml.Add(levelObjs);
+                }
+
+                gameLevelsXml.Add(goLevXml);
+            }
+            xml.Add(gameLevelsXml);
+
+            ///<Image>Images
+            XElement gameImagesXml = new XElement("Images");
+            foreach (Image go in gameProject.Images)
+            {
+                XElement goImgXml = new XElement("Image");
+                goImgXml.Add(new XElement("Name", go.Name));
+                goImgXml.Add(new XElement("FilePath", go.FilePath));
+                gameImagesXml.Add(goImgXml);
+            }
+            xml.Add(gameImagesXml);
+
+
+            ///<Setting>Settings
+            XElement gameSettingsXml = new XElement("Settings");
+            gameSettingsXml.Add(new XElement("ScreenWidth",GameSettings.ScreenWidth));
+            gameSettingsXml.Add(new XElement("ScreenHeight", GameSettings.ScreenHeight));
+            gameSettingsXml.Add(new XElement("ScreenWidth", GameSettings.BackgroundColor));
+            xml.Add(gameSettingsXml);
+
+
             System.Console.Write(xml);
 
             xml.Save(filepath);
