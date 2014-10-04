c7c0ec7af870ccb432848a59aec5ff9f35b7a3ca
Alex Patton <alexrpatton@gmail.com>
2014-10-04 12:17:17 -0400

    fix

--- SushiProject/SushiProject/Utility.cs
+++ SushiProject/SushiProject/Utility.cs
@@ -13,12 +13,6 @@ namespace SushiProject
             this.x = x;
             this.y = y;
         }
-
-        public Vector2()
-        {
-            this.x = 0;
-            this.y = 0;
-        }
     }
     class Utility
     {
